f903f276393fd0b2655c1ef8a7b567dc5b4dd993
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2024-04-27 19:35:31 -0300

    Completely rewriting the code
    A concept similar to OOP was used, by creating global tables with self
    referencing functions

--- mpv-splice.lua
+++ mpv-splice.lua
@@ -117,214 +117,468 @@
 local mp = require 'mp'
 local msg = require 'mp.msg'
 local opt = require 'mp.options'
+local utils = require 'mp.utils'
 
 --------------------------------------------------------------------------------
--- Default variables
+-- Setup config
 
-local SCRIPT_NAME = "mpv-splice"
-local default_tmp_location = "~/tmpXXX"
-local default_output_location = mp.get_property("working-directory")
+local SCRIPT_NAME = "video-splice"
 
---------------------------------------------------------------------------------
+local config = {
+    output_path = mp.get_property("working-directory"),
+    concat_file_name = "concat",
 
-local splice_options = {
-	tmp_location = os.getenv("MPV_SPLICE_TEMP") and os.getenv("MPV_SPLICE_TEMP") or default_tmp_location,
-	output_location = os.getenv("MPV_SPLICE_OUTPUT") and os.getenv("MPV_SPLICE_OUTPUT") or default_output_location
+    ffmpeg_cmd = "ffmpeg -hide_banner -loglevel warning",
+    ffmpeg_filter = "-c copy -copyts -avoid_negative_ts make_zero",
 }
-opt.read_options(splice_options, SCRIPT_NAME)
+opt.read_options(config, SCRIPT_NAME)
 
+--------------------------------------------------------------------------------
+-- Setup os dependent stuff
 
-local concat_name = "concat.txt"
+-- Not the best way to check OS, but it should work
+local _os = package.config:sub(1, 1) == "/" and "unix" or "windows"
 
-local ffmpeg = "ffmpeg -hide_banner -loglevel warning"
+system_dependent = {
+    tmp_path = _os == "unix" and "/tmp" or "%LOCALAPPDATA%\\Temp",
 
-local times = {}
-local start_time = nil
-local remove_val = ""
-
-local exit_time = 0
+    mkdir = _os == "unix" and "mkdir" or "md",
+}
 
 --------------------------------------------------------------------------------
 
-function notify(duration, ...)
-	local args = {...}
-	local text = ""
+local function notify(message, duration)
+	local duration = duration or 2
 
-	for i, v in ipairs(args) do
-		text = text .. tostring(v)
-	end
-
-	msg.info(text)
-	mp.command(string.format("show-text \"%s\" %d 1",
-		text, duration))
+	msg.info(message)
+	mp.osd_message(message, duration)
 end
 
-local function get_time()
-	local time_in_secs = mp.get_property_number('time-pos')
+local function seconds_to_clock(secs)
+	local hours = math.floor(secs / 3600)
+	local mins = math.floor((secs - hours * 3600) / 60)
+	local secs = secs - hours * 3600 - mins * 60
 
-	local hours = math.floor(time_in_secs / 3600)
-	local mins = math.floor((time_in_secs - hours * 3600) / 60)
-	local secs = time_in_secs - hours * 3600 - mins * 60
-
-	local fmt_time = string.format('%02d:%02d:%05.2f', hours, mins, secs)
-
-	return fmt_time
+    return {
+        h = hours,
+        m = mins,
+        s = secs,
+    }
 end
 
-function put_time()
-	local time = get_time()
-	local message = ""
+local function current_timestamp()
+	local seconds = mp.get_property_number('time-pos')
+    local time_data = seconds_to_clock(seconds)
 
-	if not start_time then
-		start_time = time
-		message = "[START TIMESTAMP]"
-	else
-		--times[#times+1] = {
-		table.insert(times, {
-			t_start = start_time,
-			t_end = time
-		})
-		start_time = nil
-
-		message = "[END TIMESTAMP]"
-	end
-
-	notify(2000, message, ": ", time)
+	return string.format('%02d:%02d:%05.2f', time_data.h, time_data.m, time_data.s)
 end
 
-function show_times()
-	notify(2000, "Total cuts: ", #times)
+slice_data = {
+    _start_timestamp_message = "[START TIMESTAMP]",
+    _end_timestamp_message = "[END TIMESTAMP]",
 
-	for i, obj in ipairs(times) do
-		msg.info("Slice", i, ": ", obj.t_start, " -> ", obj.t_end)
-	end
-	if start_time then
-		notify(2000, "Slice ", #times+1, " in progress.")
-	end
+    _timestamps = {},
+    _pieces = function(self)
+        return #self._timestamps
+    end,
+    pieces = function(self)
+        return self:_pieces()
+    end,
+
+    remove = {
+        _ongoing = false,
+        to_be_removed = "",
+
+        append = function(self, value)
+            self.to_be_removed = self.to_be_removed .. value
+        end,
+
+        is_ongoing = function(self)
+            return self._ongoing
+        end,
+
+        is_empty = function(self)
+            return self.to_be_removed == ""
+        end,
+
+        set_ongoing = function(self)
+            self._ongoing = true
+        end,
+
+        clear = function(self)
+            self._ongoing = false
+            self.to_be_removed = ""
+
+            notify("Exited slice deletion mode.")
+        end,
+    },
+
+    _pairs = function(self)
+        return math.floor(self:_pieces() / 2)
+    end,
+
+    _has_incomplete_slice = function(self)
+        return not (self:_pieces() % 2 == 0)
+    end,
+
+    _put_time = function(self, value)
+        table.insert(self._timestamps, value)
+    end,
+
+    _add_piece = function(self, timestamp)
+        self:_put_time(timestamp)
+
+        local message = self:_has_incomplete_slice()
+                        and self._start_timestamp_message
+                        or self._end_timestamp_message
+
+        notify(message)
+    end,
+
+    _as_pairs_coroutine_handler = nil,
+    _as_pairs_coroutine_create = function(self)
+        local function _as_pairs_coroutine_closure(self)
+            local pair = 1
+
+            for piece = 1, self:_pieces(), 2 do
+                coroutine.yield({
+                    index = pair,
+                    p_start = self._timestamps[piece],
+                    p_end = self._timestamps[piece + 1],
+                })
+
+                pair = pair + 1
+            end
+        end
+
+        self._as_pairs_coroutine_handler =
+            coroutine.create(_as_pairs_coroutine_closure)
+    end,
+
+    as_pairs = function(self)
+        self:_as_pairs_coroutine_create()
+
+        return function()
+            local _, ret = coroutine.resume(self._as_pairs_coroutine_handler, self)
+            return ret
+        end
+    end,
+
+    add_time = function(self)
+        local timestamp = current_timestamp()
+
+        slice_data:_add_piece(timestamp)
+    end,
+    
+    show_timestamps = function(self)
+        notify(string.format("Total cuts: %d", self:_pairs()))
+
+        local pair = 1
+
+        for piece = 1, self:_pieces(), 2 do
+            local t_start = self._timestamps[piece]
+            local t_end = self._timestamps[piece + 1]
+
+            msg.info(string.format("Slice %d: %s -> %s", pair, t_start, t_end))
+
+            pair = pair + 1
+        end
+
+        if self:_has_incomplete_slice() then
+            notify(string.format("Slice %d in progress.", self:_pairs() + 1))
+        end
+    end,
+
+    reset_current_slice = function(self)
+        if self:_has_incomplete_slice() then
+            notify(string.format("Slice %d reset.", self:_pairs() + 1))
+            
+            table.remove(self._timestamps)
+        end
+    end,
+
+    remove_slice = function(self)
+        local pair_index = tonumber(self.remove.to_be_removed)
+        local piece_index = pair_index * 2 - 1
+
+        if pair_index > 0 and pair_index <= self:_pairs() then
+            table.remove(self._timestamps, piece_index)
+            table.remove(self._timestamps, piece_index)
+
+            notify(string.format("Removed slice %d", pair_index))
+        end
+    end,
+
+    add_number_key_bindings = function(self)
+        -- Add shortcut keys to the interval {0..9}.
+        for i = 0, 9, 1 do
+            local key_code = "Alt+" .. i
+            local binding_name = "num_key_" .. i
+            local key_action = function()
+                self.remove:append(i)
+                notify(string.format("Slice to remove: %d", self.remove.to_be_removed), 1)
+            end
+
+            mp.add_key_binding(key_code, binding_name, key_action)
+        end
+    end,
+
+    remove_number_key_bindings = function(self)
+        for i = 0, 9, 1 do
+            mp.remove_key_binding("num_key_" .. i)
+        end
+    end,
+
+    delete_slice = function(self, index)
+        if not self.remove:is_ongoing() then
+            self.remove:set_ongoing()
+
+            notify("Entered slice deletion mode.")
+
+            self:add_number_key_bindings()
+        elseif self.remove:is_ongoing() and self.remove:is_empty() then
+            self.remove:clear()
+        else
+            self:remove_number_key_bindings()
+            self:remove_slice()
+            self.remove:clear()
+        end
+    end,
+}
+
+local quit = {
+    _exit_count = 0,
+
+    prevent_quit = function(self, pieces, name)
+        if pieces > 0 then
+            if self._exit_count > 1 then
+                mp.command(name)
+            else
+                self._exit_count = self._exit_count + 1
+            end
+
+            notify("There are timestamp pieces set. Press again to quit.", 3)
+        else
+            mp.command(name)
+        end
+    end,
+}
+
+local random = {
+    alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+
+    _started = false,
+
+	-- Better seed randomization
+    _first_startup = function(self)
+        if not self._started then
+            math.randomseed(os.time())
+            for i = 1, 3, 1 do
+                math.random()
+            end
+
+            self._started = true
+        end
+    end,
+
+    random_string = function(self, size)
+        self:_first_startup()
+
+        local rnd_str = ""
+
+        for i = 1, size, 1 do
+            local rnd_index = math.floor(math.random() * #self.alphabet + 0.5)
+            rnd_str = rnd_str .. self.alphabet:sub(rnd_index, rnd_index)
+        end
+
+        return rnd_str
+    end,
+}
+
+concat_file = {
+    _path = nil,
+    _handler = nil,
+
+    _create_file_path = function(self, tmp_path, name)
+        local name = name or "concat"
+        local file_name = string.format("%s.txt", name)
+
+        return utils.join_path(tmp_path, file_name)
+    end,
+
+    get_path = function(self)
+        return self._path
+    end,
+
+    create = function(self, tmp_path, name)
+        self._path = self:_create_file_path(tmp_path, name)
+        self._handler = io.open(self._path, "w")
+    end,
+
+    add_file = function(self, path)
+        local path_line = string.format("file '%s'\n", path)
+
+        self._handler:write(path_line)
+    end,
+
+    close = function(self)
+        self._handler:close()
+    end,
+}
+
+--------------------------------------------------------------------------------
+
+local function add_time()
+    slice_data:add_time()
 end
 
-function reset_current_slice()
-	if start_time then
-		notify(2000, "Slice ", #times+1, " reseted.")
-
-		start_time = nil
-	end
+local function show_timestamps()
+    slice_data:show_timestamps()
 end
 
-function delete_slice()
-	if remove_val == "" then
-		notify(2000, "Entered slice deletion mode.")
-
-		-- Add shortcut keys to the interval {0..9}.
-		for i=0,9,1 do
-			mp.add_key_binding("Alt+" .. i, "num_key_" .. i,
-				function()
-					remove_val = remove_val .. i
-					notify(1000, "Slice to remove: "
-						.. remove_val)
-				end
-			)
-		end
-	else
-		-- Remove previously added shortcut keys.
-		for i=0,9,1 do
-			mp.remove_key_binding("num_key_" .. i)
-		end
-
-		remove_num = tonumber(remove_val)
-		if #times >= remove_num and remove_num > 0 then
-			table.remove(times, remove_num)
-			notify(2000, "Removed slice ", remove_num)
-		end
-
-		remove_val = ""
-
-		msg.info("Exited slice deletion mode.")
-	end
+local function reset_current_slice()
+    slice_data:reset_current_slice()
 end
 
-function prevent_quit(name)
-	if start_time then
-		if os.time() - exit_time <= 2 then
-			mp.command(name)
-		else
-			exit_time = os.time()
-		end
-		notify(3000, "Slice has been marked. Press again to quit")
-	else
-		mp.command(name)
-	end
+local function delete_slice()
+    slice_data:delete_slice()
+end
+
+--------------------------------------------------------------------------------
+
+local function file_info()
+    local path = mp.get_property('path')
+    local name = mp.get_property('filename')
+    local name_without_ext = mp.get_property('filename/no-ext')
+
+    local ext = name == name_without_ext
+                and ""
+                or string.gsub(name, '.*%.(.*)$', '%1')
+
+    return {
+        path = path,
+        full_name = name,
+        name_only = name_without_ext,
+        ext = ext
+    }
+end
+
+local function make_temp_dir(tmp_path)
+    local tmp_path_name = string.format(
+        "%s_%s",
+        "video-splice-tmp", random:random_string(10)
+    )
+    local full_tmp_path = utils.join_path(tmp_path, tmp_path_name)
+
+    local mkdir_cmd = string.format(
+        "%s %s 2>&1",
+        system_dependent.mkdir, full_tmp_path
+    )
+
+    local handler = io.popen(mkdir_cmd)
+    local cmd_output = handler:read("*l")
+
+    handler:close()
+
+    if cmd_output ~= nil then
+        error(cmd_output)
+    end
+
+    return full_tmp_path
+end
+
+local function output_file_path(file_name, ext)
+    local random_string = random:random_string(10)
+    local output_file = string.format(
+        "%s_%s_cut.%s",
+        file_name, random_string, ext
+    )
+
+    return utils.join_path(config.output_path, output_file)
+end
+
+local function make_cut_path(tmp_path, piece_index, ext)
+    local random_string = random:random_string(10)
+
+    local file_name = string.format(
+        "slice_%s_%d.%s",
+        random_string, piece_index, ext
+    )
+
+    return utils.join_path(tmp_path, file_name)
+end
+
+local function run_ffmpeg_cut(piece, input_file_path, output_cut_path)
+    local cmd = string.format(
+        "%s -ss %s -i \"%s\" -to %s %s %s",
+        config.ffmpeg_cmd,
+        piece.p_start, input_file_path, piece.p_end,
+        config.ffmpeg_filter,
+        output_cut_path
+    )
+
+    os.execute(cmd)
+end
+
+local function make_timestamp_cuts(tmp_path, curr_file_path, curr_file_ext)
+    for piece in slice_data:as_pairs() do
+        local cut_path = make_cut_path(tmp_path, piece.index, curr_file_ext)
+
+        run_ffmpeg_cut(piece, curr_file_path, cut_path)
+        concat_file:add_file(cut_path)
+    end
+
+    concat_file:close()
+end
+
+local function concat_pieces(cat_file_path, output_file)
+    cmd = string.format(
+        "%s -f concat -safe 0 -i \"%s\" -c copy \"%s\"",
+        config.ffmpeg_cmd, cat_file_path, output_file
+    )
+    os.execute(cmd)
+end
+
+local function cleanup(path)
+    os.execute(string.format("rm -rf %s", path))
+    msg.info(string.format("Directory \"%s\" removed!", path))
 end
 
 function process_video()
-	local alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	local rnd_size = 10
+    local file_info = file_info()
+    local output_file = output_file_path(file_info.name_only, file_info.ext)
+    local tmp_path = make_temp_dir(system_dependent.tmp_path)
 
-	local pieces = {}
+    -- Make concat file
+    concat_file:create(tmp_path, config.concat_file_name)
 
-	-- Better seed randomization
-	math.randomseed(os.time())
-	math.random(); math.random(); math.random()
+    notify("Process started!")
 
-	if times[#times] then
-		local tmp_dir = io.popen(string.format("mktemp -d %s",
-			splice_options.tmp_location)):read("*l")
-		local input_file = mp.get_property("path")
-		local ext = string.gmatch(input_file, ".*%.(.*)$")()
+    -- Make timestamp cuts
+    make_timestamp_cuts(tmp_path, file_info.path, file_info.ext)
 
-		local rnd_str = ""
-		for i=1,rnd_size,1 do
-			local rnd_index = math.floor(math.random() * #alphabet + 0.5)
-			rnd_str = rnd_str .. alphabet:sub(rnd_index, rnd_index)
-		end
+    -- Make the concat cmd, using the concat.txt file
+    concat_pieces(concat_file:get_path(), output_file)
 
-		local output_file = string.format("%s/%s_%s_cut.%s",
-			splice_options.output_location,
-			mp.get_property("filename/no-ext"),
-			rnd_str, ext)
+    notify(string.format("File saved as: %s", output_file), 10)
+    msg.info("Process ended!")
 
-		local cat_file_name = string.format("%s/%s", tmp_dir, "concat.txt")
-		local cat_file_ptr = io.open(cat_file_name, "w")
-
-		notify(2000, "Process started!")
-
-		for i, obj in ipairs(times) do
-			local path = string.format("%s/%s_%d.%s",
-				tmp_dir, rnd_str, i, ext)
-			cat_file_ptr:write(string.format("file '%s'\n", path))
-			os.execute(string.format("%s -ss %s -i \"%s\" -to %s " ..
-				"-c copy -copyts -avoid_negative_ts make_zero \"%s\"",
-				ffmpeg, obj.t_start, input_file, obj.t_end,
-				path))
-		end
-
-		cat_file_ptr:close()
-
-		cmd = string.format("%s -f concat -safe 0 -i \"%s\" " ..
-			"-c copy \"%s\"",
-			ffmpeg, cat_file_name, output_file)
-		os.execute(cmd)
-
-		notify(10000, "File saved as: ", output_file)
-		msg.info("Process ended!")
-
-		os.execute(string.format("rm -rf %s", tmp_dir))
-		msg.info("Temporary directory removed!")
-	end
+    -- Cleanup
+    cleanup(tmp_path)
 end
 
 mp.set_property("keep-open", "yes") -- Prevent mpv from exiting when the video ends
 mp.set_property("quiet", "yes") -- Silence terminal.
 
 mp.add_key_binding('q', "quit", function()
-	prevent_quit("quit")
+	quit:prevent_quit(slice_data:pieces(), "quit")
 end)
 mp.add_key_binding('Shift+q', "quit-watch-later", function()
-	prevent_quit("quit-watch-later")
+	quit:prevent_quit(slice_data:pieces(), "quit-watch-later")
 end)
 
-mp.add_key_binding('Alt+t', "put_time", put_time)
-mp.add_key_binding('Alt+p', "show_times", show_times)
+mp.add_key_binding('Alt+t', "put_time", add_time)
+mp.add_key_binding('Alt+p', "show_times", show_timestamps)
 mp.add_key_binding('Alt+c', "process_video", process_video)
 mp.add_key_binding('Alt+r', "reset_current_slice", reset_current_slice)
 mp.add_key_binding('Alt+d', "delete_slice", delete_slice)
